c3b303327f4f232113ac8d9e4e2cff1a62dfdcac
gamal ahmed <gamalahmed3265@gmail.com>
2023-02-03 00:16:02 +0200

    add longestCommonPrefix.py

---
+++ longestCommonPrefix.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        # Longest common prefix string
+        result = ""
+        # Base condition
+        if strs is None or len(strs) == 0:
+            return result
+        # If there is only one element in array then return it
+        if len(strs) == 1:
+            return strs[0]
+        # Find the minimum length string from the array
+        minimum_length = len(strs[0])
+        for i in range(1, len(strs)):
+            minimum_length = min(minimum_length, len(strs[i]))
+        # Loop until the minimum length
+        for i in range(0, minimum_length):
+            # Get the current character from the first string
+            current = strs[0][i]
+            # Check if this character is found in all other strings or not
+            for j in range(0, len(strs)):
+                if strs[j][i] != current:
+                    return result
+            result += current
+        return result

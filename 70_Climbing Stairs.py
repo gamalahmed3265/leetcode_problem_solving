049bd61ad9c86040f777659eed6c6728650466b2
gamal ahmed <gamalahmed3265@gmail.com>
2023-02-18 23:17:34 +0200

    add 70_Climbing Stairs.py

---
+++ 70_Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        prev1 = 1  # dp[i - 1]
+        prev2 = 1  # dp[i - 2]
+
+        for _ in range(2, n + 1):
+            dp = prev1 + prev2
+            prev2 = prev1
+            prev1 = dp
+
+        return prev1

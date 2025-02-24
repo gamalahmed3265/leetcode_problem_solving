32048868a1242cfaf8658220cd8d0cb1c16f975d
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2025-02-24 19:33:47 +0200

    Add files via upload

---
+++ 118. Pascal's Triangle/main.py
@@ -0,0 +1,18 @@
+def generate(numRows):
+    if numRows == 0:
+        return []
+    
+    triangle = [[1]]
+    
+    for i in range(1, numRows):
+        prev_row = triangle[-1]
+        current_row = [1]
+        
+        for j in range(1, i):
+            current_row.append(prev_row[j-1] + prev_row[j])
+        
+        current_row.append(1)
+        triangle.append(current_row)
+    
+    return triangle
+print(generate(2))

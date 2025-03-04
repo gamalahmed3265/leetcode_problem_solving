31c3133bdf4f71c5d6a0280d9412650151167de5
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2025-03-04 13:24:41 +0200

    Update main.py

--- 121  Best Time to Buy and Sell Stock/main.py
+++ 121  Best Time to Buy and Sell Stock/main.py
@@ -6,15 +6,3 @@ for price in prices:
     max_price=max(max_price,price-min_price)
 print(max_price)
 
-
-def maxProfit(prices):
-    max_profit = 0  # Initialize max_profit to 0
-
-    for price in prices:
-        # Update min_price if the current price is smaller
-        min_price = min(min_price, price)
-        
-        # Update max_profit if the current profit is larger
-        max_profit = max(max_profit, price - min_price)
-
-    return max_profit
